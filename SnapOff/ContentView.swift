dbed9866b90b59f299db9f0a704edb77085ec57d
Dheera <dulsarak@gmail.com>
2023-09-15 00:03:33 -0700

    changed the text

--- SnapOff/ContentView.swift
+++ SnapOff/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("This is a test!")
+            Text("This test!")
         }
         .padding()
     }
