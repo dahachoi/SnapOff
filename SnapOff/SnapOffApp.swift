7e5419e53d0ddd04a98768bfc5d0cdeed742e66e
Daha Choi <daha.choi@gmail.com>
2023-09-14 23:46:04 -0700

    Initial Commit

---
+++ SnapOff/SnapOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapOffApp.swift
+//  SnapOff
+//
+//  Created by Daha Choi on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct SnapOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
