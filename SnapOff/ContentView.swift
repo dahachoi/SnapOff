4907c93c2b4a8e55a7ef34dd9fd2a41738df5896
Dheera <dulsarak@gmail.com>
2023-09-15 00:11:54 -0700

    changed text to hidden message

--- SnapOff/ContentView.swift
+++ SnapOff/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Dheera is quite fat!")
+            Text("Daha is whore")
         }
         .padding()
     }
