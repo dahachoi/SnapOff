ac52c7e50c631f8439ac24d7c606e26a66eb6691
Dheera <dulsarak@gmail.com>
2023-09-15 00:16:14 -0700

    another message

--- SnapOff/ContentView.swift
+++ SnapOff/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Daha is whore")
+            Text("Daha is a niga")
         }
         .padding()
     }
