fc7afaa5594b0cfecc13ad7ee6404d870ae616b7
Dheera <dulsarak@gmail.com>
2023-09-15 00:05:39 -0700

    changed text to this is crazy

--- SnapOff/ContentView.swift
+++ SnapOff/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("This test!")
+            Text("This is crazy!")
         }
         .padding()
     }
